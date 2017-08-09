0234a9dc2e2de772534c550d488895f6e8ba10eb
sharbeen <chappisurf@gmail.com>
2017-08-09 21:49:07 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,10 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight()
 {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick=function(){
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 10);
     img.style.marginLeft ='100px';
 };
