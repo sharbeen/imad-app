83f802525b71215a0150b7863027ec2faf8586a3
sharbeen <chappisurf@gmail.com>
2017-08-09 21:22:36 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "NEW VALUE";
+var img = documennt.getElementById("madi");
+img.onclick=function(){
+    img.style.marginLeft = '100px';
+};
