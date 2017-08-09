c03607512eb2cd25cd3199ba3254749059dd223d
sharbeen <chappisurf@gmail.com>
2017-08-09 21:37:54 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,14 @@
 console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTML = "NEW VALUE";
-var img = documennt.getElementById("madi");
+var element = document.getElementById('main-text');
+element.innerHTML = 'NEW VALUE';
+var img = documennt.getElementById('madi');
+var marginLeft = 0;
+function moveRight()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick=function(){
+    var interval = setInterval(moveRight, 100);
     img.style.marginLeft ='100px';
 };
