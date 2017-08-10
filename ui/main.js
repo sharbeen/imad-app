87b3fc268ac4f89f4f0c677ee6c43d19db882e29
sharbeen <chappisurf@gmail.com>
2017-08-10 23:49:04 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 var button = document.getElementById('counter');
-button.onClick = function(){
+button.onclick = function(){
     
     counter = counter + 1;
     var span = document.getElementById('count');
