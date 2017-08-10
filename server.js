5ab991e8c81ff500a0545ca34a820d05a07501ca
sharbeen <chappisurf@gmail.com>
2017-08-10 11:34:32 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,11 +83,11 @@ app.get('/:articleName', function(req,res){
    res.send(createTemplate(articles[articleName]));
 });
 
-var adder =0;
-app.get('/adder', function (req, res) {
+var counter =0;
+app.get('/counter', function (req, res) {
     
-adder = adder + 1;
-res.send(adder.toString());
+counter = counter + 1;
+res.send(counter.toString());
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
