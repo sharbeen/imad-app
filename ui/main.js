5403e71d12e2b83563a757932e551ae0707c6065
sharbeen <chappisurf@gmail.com>
2017-08-11 11:19:53 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,27 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
+    //Make a request to counter end point
+    //create a request object
+    var request = new XMLHttpRequest();
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+      if(request.readystate==XMLHttprequest.DONE){
+          //take some action
+          if(request.status==200)
+          {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+          }
+      }  
+      //not done yet
+    };
+    //make the request
+    //render the variable in the correct span
+    
+    request.open('GET','http://chappisurf.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
