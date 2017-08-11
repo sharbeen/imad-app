2e267d3a510634259166fa3c802fcbf66a027f4f
sharbeen <chappisurf@gmail.com>
2017-08-11 20:14:47 +1000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,17 +27,36 @@ button.onclick = function (){
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function ()
 {
-    var names = ['name1', 'name2', 'name3'];
-    var list = '';
-    for(var i=0; i< names.length; i++) {
-        
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+     var request = new XMLHttpRequest();
+    
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE){
+          //take some action
+          if(request.status === 200)
+          {
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for(var i=0; i< names.length; i++) {
+                list += '<li>' + names[i] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+          }
+      }  
+      //not done yet
+    };
+    //make the request
+    //render the variable in the correct span
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET', 'http://chappisurf.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
+   
 };
