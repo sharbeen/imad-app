59a387f79e00208a40d8e170fb2841199d957496
sharbeen <chappisurf@gmail.com>
2017-08-11 20:01:13 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,9 +103,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function (req, res)
+app.get('/submit-name', function (req, res)
 {
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     //JSON
     res.send(JSON.stringify(names));
