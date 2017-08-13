ad3d643d3437f2d6a5067cd4d1f970c7cb7039c1
sharbeen <chappisurf@gmail.com>
2017-08-13 23:29:35 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'chappisurf',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'process.env.DB_PASSWORD'
+    password: 'db-chappisurf-93411'
 } 
 
 var app = express();
