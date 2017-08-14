a728f9206b474ca425f96d68c30074dde0006803
sharbeen <chappisurf@gmail.com>
2017-08-14 17:19:18 +1000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.listen(port, function () {
 
 app.get('/articles/:articleName', function (req, res) {
     
-   pool.query("SELECT * FROM article WHERE title =" + req.params.articleName, function(err,result)
+   pool.query("SELECT * FROM article WHERE title ='" + req.params.articleName+"'", function(err,result)
    {
       if(err)
       {
